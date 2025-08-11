be8eb7459d415a57e70cd1ddf38ea8e70c73b1bc
JoaoMesquitaL <joao.pedroml@protonmail.com>
2025-08-11 20:20:27 -0300

    Implementado variaveis para os valores capturados do texto + adaptação regex val unit

--- index.js
+++ index.js
@@ -18,6 +18,17 @@ var pginicio = 1; //incio da página a ser convertida
 var pgfim = 1; //fim da página a ser convertida
 var docPdf = "pdfs/pedido15.pdf" //caminho do PDF a ser convertido
 
+//Definindo variaveis do Pedido
+var nomeCliente = "";
+var itemPedido  = "";
+var vlUnitItem  = "";
+var dtEntrega = "";
+var vlTotal  = "";
+var obsPedido  = "";
+var formaPagto  = "";
+var vlFrete  = "";
+var qtdItem = "";
+
 const file = docPdf; //caminho do PDF a ser convertido
 const pd = new Poppler(); //instanciando o Poppler para conversão de PDF para PNG
 const options = { //passando as variaveis de configuração para o poppler
@@ -48,7 +59,8 @@ async function main() { //definição de funcção assíncrona para executar as 
         const regexNome = /DADOS\s*DO\s*CLIENTE[\s\S]*?Nome[:\s]*([^\n\r]{1,33})/i;
         const matchNome = text.match(regexNome);
         if (matchNome) {
-            console.log("Nome do cliente encontrado:", matchNome[1]);
+            var nomeCliente = matchNome[1].trim();
+            console.log("Nome do cliente encontrado:", nomeCliente);
         } else {
             console.log("Nome do cliente não encontrado.");
         }
@@ -57,17 +69,19 @@ async function main() { //definição de funcção assíncrona para executar as 
         const regexItemPed = /Descrição.*(?:\r?\n){2}\s*([^\n\r]{1,15})/i;
         const matchItemPed = text.match(regexItemPed);
         if (matchItemPed) {
-            console.log("Item do pedido encontrado: ", matchItemPed[1]);
+            var itemPedido  = matchItemPed[1].trim();
+            console.log("Item do pedido encontrado: ", itemPedido);
         } else {
             console.log("Item do pedido não encontrado.");
         }
     
 
     //Buscando valor unitário do produto
-        const regexValUnit = /Valor Unit[aá]rio[\s\S]*?(?:\r?\n){2}[^\r\n]*?(R\$\s?\d{1,3},\d{2})/
+        const regexValUnit = /Valor Unit[aá]rio[\s\S]*?(?:\r?\n){2}[^\r\n]*?R\$\s?([\d.]+,\d{2})/
         const matchValUnit = text.match(regexValUnit);
         if (matchValUnit){
-            console.log("Valor unitario do Pedido encontrado: ", matchValUnit[1]);
+            var vlUnitItem  = matchValUnit[1].trim();
+            console.log("Valor unitario do Pedido encontrado: ", vlUnitItem);
         } else {
             console.log("Valor unitário do item não encontrado");
         }
@@ -76,7 +90,8 @@ async function main() { //definição de funcção assíncrona para executar as 
         const regexData = /Data\s*entrega[:\s]*([0-3]?\d\/[01]?\d\/\d{2,4})/i;
         const matchData = text.match(regexData);
         if (matchData) {
-            console.log("Data de Entrega encontrada:", matchData[1]);
+            var dtEntrega = matchData[1];
+            console.log("Data de Entrega encontrada:", dtEntrega);
         } else {
             console.log("Data de Entrega não encontrada.");
         }
@@ -85,7 +100,8 @@ async function main() { //definição de funcção assíncrona para executar as 
         const regexValor =  /TOTAL\s*R\$?\s*([\d\.,]+)/i; //expressão regular para encontrar o valor com base na palavra "TOTAL" usando /i para ignorar maiúsculas e minúsculas
         const matchValt = text.match(regexValor); //aplicando o regex no texto reconhecido e usando a função match para encontrar o valor
         if (matchValt) { //se o retorno da função match for verdadeiro, retorna o valor encontrado
-            console.log("Valor TOTAL encontrado:", matchValt[1]); //linha inteira do regex (com a cifra e espaço)
+            var vlTotal = matchValt[1];
+            console.log("Valor TOTAL encontrado:", vlTotal); //linha inteira do regex (com a cifra e espaço)
            // console.log("Apenas o valor:", match[1]); //apenas o valor (sem a cifra e espaço) 
         } else {
             console.log("Valor TOTAL não encontrado."); //retorno caso não encontre o valor
@@ -95,7 +111,8 @@ async function main() { //definição de funcção assíncrona para executar as 
         const regexObs = /Observa(?:ções|gées):\s*([^\n\r]*)/i;
         const matchObs = text.match(regexObs);
         if (matchObs) {
-            console.log("Observações encontradas:", matchObs[1].trim());
+            var obsPedido = matchObs[1].trim();
+            console.log("Observações encontradas:", obsPedido);
         } else {
             console.log("Observações do pedido não encontradas.");
         }
@@ -104,7 +121,8 @@ async function main() { //definição de funcção assíncrona para executar as 
         const regexPagamento = /Forma de Pagamento:\s*([^\n\r]*)/i;
         const matchPagamento = text.match(regexPagamento);
         if (matchPagamento) {
-            console.log("Forma de Pagamento encontrada:", matchPagamento[1].trim());
+            var formaPagto = matchPagamento[1].trim();
+            console.log("Forma de Pagamento encontrada:", formaPagto);
         } else {
             console.log("Forma de Pagamento não encontrada.");
         }
@@ -113,7 +131,8 @@ async function main() { //definição de funcção assíncrona para executar as 
         const regexFrete = /Frete\s*R\$?\s*([\d\.,]+)/i;
         const matchFrete = text.match(regexFrete);
         if (matchFrete) {
-            console.log("Valor do Frete encontrado:", matchFrete[1].trim());
+            var vlFrete = matchFrete[1].trim();
+            console.log("Valor do Frete encontrado:", vlFrete);
         } else {
             console.log("Valor do Frete não encontrado.");
         }
@@ -122,7 +141,8 @@ async function main() { //definição de funcção assíncrona para executar as 
         const regexQuantIT = /(\d+)\s*unid/i;
         const matchQuantIT = text.match(regexQuantIT);
         if(matchQuantIT) {
-            console.log("Quantidade do item encontrada: ", matchQuantIT[1])
+            var qtdItem = matchQuantIT[1].trim();
+            console.log("Quantidade do item encontrada: ", qtdItem);
         } else {
             console.log("Quantidade do item não encontrada.");
         }
